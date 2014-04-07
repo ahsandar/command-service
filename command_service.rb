5189149085c901521b55dad7c9055f85a7291e83
Ahsan Dar <ahsan.dar@bauermedia.co.uk>
2014-04-07 10:33:18 +0100

    removed directory code irrelevant to the command class

--- command_service.rb
+++ command_service.rb
@@ -45,59 +45,6 @@ class CommandService
     @log.timestamp
   end
 
-  def check_out_assets(file_assets, local_asset_path)
-    file_assets.each do |file|
-      file_name = File.join(local_asset_path, file.value)
-      cms_file_path = file.cms_file_path
-      @log.msg "copied #{cms_file_path}, #{file_name}"
-      FileUtils.cp cms_file_path, file_name
-    end
-  end
-
-  def remove_entry_secure(path)
-    @log.msg "Removing entry secure: #{path}"
-    if_exist = (Dir.exist?(path) || File.exists?(path))
-    FileUtils.remove_entry_secure(path) if (if_exist && !DEBUG_MODE)
-    log.msg "Removed entry secure: #{path}"
-  end
-
-  def remove_dir(folder)
-    @log.msg "Removing dir folder: #{folder}"
-    FileUtils.remove_dir folder if (Dir.exist?(folder) && !DEBUG_MODE)
-    @log.msg "Removed dir folder: #{folder}"
-    folder
-  end
-
-  def self.full_directory_path(folder)
-    folder_path = File.join(Dir.getwd, folder)
-    folder_path
-  end
-
-  def create_path_location(path)
-    dir_path = File.expand_path(path)
-    @log.msg "created path location: #{dir_path}"
-    dir_path
-  end
-
-  def create_randomized_location(path)
-    dir_path = File.join(path, random_sub_path, random_sub_path)
-    FileUtils.mkdir_p dir_path
-    @log.msg "created randomized location: #{dir_path}"
-    dir_path
-  end
-
-
-  def copy_target_to_destination(source, destination)
-    FileUtils.remove_entry_secure(destination) if File.exists?(destination)
-    FileUtils.mkdir_p File.dirname(destination)
-    FileUtils.cp_r source, destination
-  end
-
-  def random_sub_path
-    o =  [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten
-    string  =  (0...5).map{ o[rand(o.length)] }.join
-  end
-
   private
 
   def command
